aa40cf82982151c0905039bb95a77153559d0105
Okke van der Wal <okkevanderwal@gmail.com>
2019-04-08 20:45:19 +0200

    Adding the tokinizer.

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,5 +1,17 @@
 #Preprocessing steps for Open NMT
 
+#imports
+
+import tensorflow as tf
+
+
 #Tokenization
 
-#
+#By default, Keras’ Tokenizer will trim out all the punctuations
+en_tokenizer = tf.keras.preprocessing.text.Tokenizer(filters='')
+en_tokenizer.fit_on_texts(#raw_data_name)
+
+# raw English sentences converted to integer sequences
+data_en = en_tokenizer.texts_to_sequences(raw_data_en)
+
+
